dc3c97477015fea6e3476623500243c2f6123c9d
Cristiano Schroeder <84641175+cristianosch@users.noreply.github.com>
2023-09-13 17:45:13 +0100

    Update settings.py

--- aws/settings.py
+++ aws/settings.py
@@ -21,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+#SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-&r#wz0+klp=r()4az*2qsfamyjsr44-650@6yv-f^7q2u!d%k0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+#DEBUG = config('DEBUG', cast=bool)
+DEBUG=True
 
 ALLOWED_HOSTS = []
 
